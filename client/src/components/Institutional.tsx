bf6c833a5de79c5fd4546ba431875708c55ea378
ti-pskeromais <tech@pskeromais.com.br>
2026-03-18 15:30:58 -0300

    fixing text boxes of institutional

--- client/src/components/Institutional.tsx
+++ client/src/components/Institutional.tsx
@@ -29,20 +29,22 @@ export default function Institutional() {
 
   return (
     <section id="institucional" className="w-full pb-20 md:p-0 bg-white" >
-      <div className="w-full bg-blue-500 text-center py-10" style={{backgroundImage: 'url("' + bginstitucional + '")' , backgroundSize: '100% auto'}}>
-        <img src={escudo} alt="Escudo de Proteção" style={{maxWidth: 130}} className="w-full mx-auto mb-4" />
-        <h2 className="text-white font-bold">Quem Somos</h2>
-        <br />
-        <p className="indent-8 text-lg text-white mb-4 text-justify rounded-md p-3 m-2" style={{backgroundColor: '#020237'}}>
-          A <b>Panamérica Seguros</b> é uma empresa com uma história de sucesso que começou em <b>1988</b>, graças à visão e determinação do seu fundador, <b>Emanoel Ribamar Alencar Lima</b>. 
-          Nascido em uma família grande, Emanoel iniciou sua carreira como gerente de banco e, aos <b>38 anos</b>, decidiu criar seu próprio negócio de seguros. 
-        </p>
-        <p className="indent-8 text-lg text-white text-justify rounded-md p-3 m-2" style={{backgroundColor: '#020237'}}>
-          Hoje, a <b>Panamérica</b> é uma referência no mercado, com uma carteira de clientes relevantes, nas áreas <b>industrial</b>, <b>comercial</b> e <b>empresas de ônibus</b>, além de forte atuação no <b>segmento individual</b>. 
-          Com uma trajetória marcada pela dedicação e espírito empreendedor, a Panamérica continua a crescer. <br />
-          Mais do que vender seguros, oferecemos um <b>atendimento humanizado</b> e uma <b>consultoria especializada</b>, garantindo que você tenha <b>a cobertura certa para cada momento da sua vida</b>.
-          Sempre com visão inovadora, a <b>Panamérica</b> está na palma da sua mão oferecendo um site completo, onde você pode fazer a simulação do seu seguro e usufruir de todos os nossos serviços. <b>Panamérica seguros</b>, proteção sem fronteiras.
-        </p>
+      <div className="w-full bg-blue-500 text-center py-10" style={{backgroundImage: 'url("' + bginstitucional + '")' , backgroundSize: 'auto 100%'}}>
+        <span style={{maxWidth: 600 , margin: '0 auto' , display: 'block'}}>
+          <img src={escudo} alt="Escudo de Proteção" style={{maxWidth: 130}} className="w-full mx-auto mb-4" />
+          <h2 className="text-white font-bold">Quem Somos</h2>
+          <br />
+          <p className="indent-8 text-lg text-white mb-4 text-justify rounded-md p-3 m-2" style={{backgroundColor: '#020237'}}>
+            A <b>Panamérica Seguros</b> é uma empresa com uma história de sucesso que começou em <b>1988</b>, graças à visão e determinação do seu fundador, <b>Emanoel Ribamar Alencar Lima</b>. 
+            Nascido em uma família grande, Emanoel iniciou sua carreira como gerente de banco e, aos <b>38 anos</b>, decidiu criar seu próprio negócio de seguros. 
+          </p>
+          <p className="indent-8 text-lg text-white text-justify rounded-md p-3 m-2" style={{backgroundColor: '#020237' }}>
+            Hoje, a <b>Panamérica</b> é uma referência no mercado, com uma carteira de clientes relevantes, nas áreas <b>industrial</b>, <b>comercial</b> e <b>empresas de ônibus</b>, além de forte atuação no <b>segmento individual</b>. 
+            Com uma trajetória marcada pela dedicação e espírito empreendedor, a Panamérica continua a crescer. <br />
+            Mais do que vender seguros, oferecemos um <b>atendimento humanizado</b> e uma <b>consultoria especializada</b>, garantindo que você tenha <b>a cobertura certa para cada momento da sua vida</b>.
+            Sempre com visão inovadora, a <b>Panamérica</b> está na palma da sua mão oferecendo um site completo, onde você pode fazer a simulação do seu seguro e usufruir de todos os nossos serviços. <b>Panamérica seguros</b>, proteção sem fronteiras.
+          </p>
+        </span>
       </div>
       <div className="container mx-auto pt-5 px-4">
 
